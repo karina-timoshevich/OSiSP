88120731010805ddc2d5eddbe682f0ecefcdb3fa
Karina Timoshevich <karina03tima@gmail.com>
2024-11-13 00:10:24 +0300

    LabRab5 files

---
+++ LabRab5_Timoshevich_OSiSP/client.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <winsock2.h>
+#include <thread>
+#include <string>
+#pragma comment(lib, "ws2_32.lib")
+
+#define PORT 12345
+#define BUFFER_SIZE 1024
+
+SOCKET client_socket;
+
+void receive_messages() {
+    char buffer[BUFFER_SIZE];
+    while (true) {
+        int recv_size = recv(client_socket, buffer, BUFFER_SIZE, 0);
+        if (recv_size <= 0) {
+            std::cout << "Connection lost with the server\n";
+            break;
+        }
+        buffer[recv_size] = '\0';
+        std::cout << "\nNew message: " << buffer << "\n";
+    }
+}
+
+int main() {
+    WSADATA wsaData;
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+    client_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); // Server address
+    server_addr.sin_port = htons(PORT);
+
+    if (connect(client_socket, (sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        std::cerr << "Failed to connect to the server.\n";
+        return 1;
+    }
+
+    std::string name;
+    std::cout << "Enter your name: ";
+    std::getline(std::cin, name);
+    send(client_socket, name.c_str(), name.size(), 0);
+    std::thread receiver(receive_messages);
+    receiver.detach();
+
+    while (true) {
+        std::string receiver_name, message;
+        std::cout << "Enter recipient's name: ";
+        std::getline(std::cin, receiver_name);
+        std::cout << "Enter your message: ";
+        std::getline(std::cin, message);
+        std::string full_message = name + "|" + receiver_name + "|" + message;
+        send(client_socket, full_message.c_str(), full_message.size(), 0);
+    }
+
+    closesocket(client_socket);
+    WSACleanup();
+    return 0;
+}
--- LabRab5_Timoshevich_OSiSP/server.cpp
+++ LabRab5_Timoshevich_OSiSP/server.cpp
@@ -1,3 +1,88 @@
-//
-// Created by User on 08.11.2024.
-//
+#include <iostream>
+#include <winsock2.h>
+#include <map>
+#include <thread>
+#include <string>
+#include <sstream>
+#include <queue>
+#pragma comment(lib, "ws2_32.lib")
+
+#define PORT 12345
+#define BUFFER_SIZE 1024
+
+std::map<SOCKET, std::string> clients;
+std::map<std::string, std::queue<std::string>> message_queue;
+
+void send_to_client(const std::string& message, const std::string& receiver_name) {
+    bool found = false;
+    for (const auto& client : clients) {
+        if (client.second == receiver_name) {
+            send(client.first, message.c_str(), message.size(), 0);
+            found = true;
+            break;
+        }
+    }
+
+    if (!found) {
+        message_queue[receiver_name].push(message);
+    }
+}
+
+void handle_client(SOCKET client_socket) {
+    char buffer[BUFFER_SIZE];
+    std::string client_name;
+    int recv_size = recv(client_socket, buffer, BUFFER_SIZE, 0);
+    if (recv_size > 0) {
+        buffer[recv_size] = '\0';
+        client_name = buffer;
+        clients[client_socket] = client_name;
+        std::cout << client_name << " connected\n";
+        while (!message_queue[client_name].empty()) {
+            send(client_socket, message_queue[client_name].front().c_str(), message_queue[client_name].front().size(), 0);
+            message_queue[client_name].pop();
+        }
+    }
+
+    while (true) {
+        int recv_size = recv(client_socket, buffer, BUFFER_SIZE, 0);
+        if (recv_size <= 0) {
+            std::cout << client_name << " disconnected\n";
+            closesocket(client_socket);
+            clients.erase(client_socket);
+            break;
+        }
+        buffer[recv_size] = '\0';
+
+        std::stringstream ss(buffer);
+        std::string sender, receiver, message;
+        std::getline(ss, sender, '|');
+        std::getline(ss, receiver, '|');
+        std::getline(ss, message);
+        std::string full_message = sender + ": " + message;
+        send_to_client(full_message, receiver);
+    }
+}
+
+int main() {
+    WSADATA wsaData;
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+    SOCKET server_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
+    bind(server_socket, (sockaddr*)&server_addr, sizeof(server_addr));
+    listen(server_socket, SOMAXCONN);
+
+    std::cout << "Server started and waiting for connections...\n";
+
+    while (true) {
+        SOCKET client_socket = accept(server_socket, nullptr, nullptr);
+        std::thread client_thread(handle_client, client_socket);
+        client_thread.detach();
+    }
+
+    closesocket(server_socket);
+    WSACleanup();
+    return 0;
+}
