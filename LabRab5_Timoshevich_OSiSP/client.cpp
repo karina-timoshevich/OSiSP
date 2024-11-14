76ccf41da51a533ee319e50003b5465a74b51a34
Karina Timoshevich <karina03tima@gmail.com>
2024-11-14 14:57:59 +0300

    add report for lab5

--- LabRab5_Timoshevich_OSiSP/client.cpp
+++ LabRab5_Timoshevich_OSiSP/client.cpp
@@ -29,7 +29,7 @@ int main() {
 
     sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); // Server address
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     server_addr.sin_port = htons(PORT);
 
     if (connect(client_socket, (sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
